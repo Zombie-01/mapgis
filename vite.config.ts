c640625411367a60505158ad9305e587e3aba464
adiya <mm6816557@gmail.com>
2026-05-28 00:30:09 +0800

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+  const env = loadEnv(mode, ".", "");
+  return {
+    server: {
+      port: 3000,
+      host: "0.0.0.0",
+    },
+    plugins: [],
+    define: {
+      "process.env.VITE_GOOGLE_MAPS_API_KEY": JSON.stringify(
+        env.VITE_GOOGLE_MAPS_API_KEY,
+      ),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
       },
-      plugins: [],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
